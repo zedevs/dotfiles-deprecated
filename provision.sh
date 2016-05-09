8fb709f64509b54af5d3cde2c497c7ad390f373a
Zdravko Evstatiev <zevstatiev@gmail.com>
2016-05-09 18:31:09 +0300

    Move installation to bash

--- provision.sh
+++ provision.sh
@@ -18,10 +18,31 @@ sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 echo `which zsh` | sudo tee -a /etc/shells
 chsh -s `which zsh`
 
-# configure nvim
-mkdir ~/.config
-ln -s ~/.vim ~/.config/nvim
-ln -s ~/.vimrc ~/.config/nvim/init.vim
+# link files
+current_dir="$(dirname $(readlink -f $0))"
+mkdir $HOME/.config
+ln -s $current_dir/vim $HOME/.config/nvim
+ln -s $current_dir/vimrc $HOME/.config/nvim/init.vim
+ln -s $current_dir/vim $HOME/.vim
+ln -s $current_dir/gitconfig $HOME/.gitconfig
+ln -s $current_dir/gitconfig.private $HOME/.gitconfig.private
+ln -s $current_dir/script $HOME/.script
+ln -s $current_dir/zshrc $HOME/.zshrc
+ln -s $current_dir/zsh $HOME/.zsh
+ln -s $current_dir/bashrc $HOME/.bashrc
+ln -s $current_dir/bash $HOME/.bash
+
+# pull nvim plugins
+git submodule init
+git submodule update
+
+# install nvim plugins with Vundle
+nvim +PluginInstall +qall
+
+# compile ctrlp-cmatcher
+export CFLAGS=-Qunused-arguments
+export CPPFLAGS=-Qunused-arguments
+bash $HOME/.vim/bundle/ctrlp-cmatcher/install.sh
 
 # Cleanup old downloads
 brew cleanup
